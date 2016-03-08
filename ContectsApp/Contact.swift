78d334857dfa4ee819a36a6fa7b7eb895f2a6b98
Jinyu Liu <jliu@sch.org>
2016-03-08 10:57:27 -0500

    3/8

---
+++ ContectsApp/Contact.swift
@@ -0,0 +1,29 @@
+//
+//  Contect.swift
+//  ContectsApp
+//
+//  Created by Jinyou Liu on 3/8/16.
+//  Copyright © 2016 Electricfish Electroincs. All rights reserved.
+//
+
+import Foundation
+
+class Contact{
+    var firstName:String
+    var lastName:String
+    var number:String
+    var lastNameFirst = false
+    init(firstName:String,lastName:String,number:String){
+        self.firstName = firstName
+        self.lastName = lastName
+        self.number = number
+    }
+    
+    func getName() -> String{
+        if(lastNameFirst){
+            return firstName + " " + lastName
+        }else{
+            return lastName + " " + firstName
+        }
+    }
+}
